a7e359b957ae0836d638c57f781bd1e80daa2c4b
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-03-21 13:39:14 +0000

    Problems Complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,37 @@
-public class PracticeProblem {
-
-	public static void main(String args[]) {
-
+public class PracticeProblem{
+	static void selectionSortName(String[] names, int[] ages){
+		for(int i = 0; i < names.length; ++i){
+			int smallestIndex = i;
+			for(int j = i+1; j < names.length; ++j){
+				if(names[j].compareToIgnoreCase(names[smallestIndex]) < 0){
+					smallestIndex = j;
+				}
+			}
+			String temp = names[i];
+			int temp1 = ages[i];
+			names[i] = names[smallestIndex];
+			ages[i] = ages[smallestIndex];
+			names[smallestIndex] = temp;
+			ages[smallestIndex] = temp1;
+		}
 	}
 
-	
+	static void selectionSortAge(String[] names, int[] ages){
+		for(int i = 0; i < ages.length; ++i){
+			int smallestIndex = i;
+			for(int j = i+1; j < ages.length; ++j){
+				if(ages[j] < ages[smallestIndex]){
+					smallestIndex = j;
+				}
+			}
+			String temp = names[i];
+			int temp1 = ages[i];
+			names[i] = names[smallestIndex];
+			ages[i] = ages[smallestIndex];
+			names[smallestIndex] = temp;
+			ages[smallestIndex] = temp1;
+		}
+	}
 
+	public static void main(String args[]){}
 }
